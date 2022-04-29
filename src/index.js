86735dfd46a46754983bf4f8b22ec9b2f120f100
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-04-29 10:29:59 +0300

    Add stuff from searchlib

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-const applyConfig = (config) => {
+import installGlobalsearch from './config';
+
+export default (config) => {
+  config.settings.searchlib = installGlobalsearch(config.settings.searchlib);
+
+  const { globalsearch } = config.settings.searchlib.searchui;
+
+  // Tweak the searchlib config to use the middleware instead of the index
+  globalsearch.elastic_index = '_es/globalsearch';
+
   return config;
 };
-
-export default applyConfig;
