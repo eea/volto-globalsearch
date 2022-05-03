af84cf80adecb402ee32e248a1939f32342e35cf
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2022-05-03 17:20:18 +0300

    enable nlp for globalsearch

--- src/config/global-search-config.js
+++ src/config/global-search-config.js
@@ -17,7 +17,7 @@ const globalSearchConfig = {
   title: 'Global search and catalogue',
   layoutComponent: 'FilterAsideLayout',
   contentBodyComponent: 'FilterAsideContentView',
-  enableNLP: false, // enables NLP capabilities
+  enableNLP: true, // enables NLP capabilities
   facetsListComponent: 'VerticalCardsModalFacets',
   runtime_mappings: build_runtime_mappings(clusters),
   useSearchPhrases: false,
