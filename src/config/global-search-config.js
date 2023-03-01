f6d7dc4f787005027a102fb513aa2e8aba5ffba9
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2023-03-01 15:28:42 +0200

    removed unused imports

--- src/config/global-search-config.js
+++ src/config/global-search-config.js
@@ -1,20 +1,5 @@
 import cloneDeep from 'lodash.clonedeep';
 
-import { build_runtime_mappings } from '../utils';
-import clusterParams, { clusters } from './clusters';
-
-import facets from './facets';
-import views from './views';
-import query from './query';
-import filters from './filters';
-import download from './download';
-import vocabs from './vocabulary';
-
-import objectProvidesWhitelist from './json/objectProvidesWhitelist.json';
-import spatialWhitelist from './json/spatialWhitelist.json';
-import placesBlacklist from './json/placesBlacklist.json';
-import typesWhitelist from './json/typesWhitelist.json';
-
 import globalSearchBaseConfig from './global-search-base-config.js';
 
 let globalSearchConfig = cloneDeep(globalSearchBaseConfig);
