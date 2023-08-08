bdeaaeff51ab11cd704fe7bd07e0169a7bed46f5
ana-oprea <anaop3212@gmail.com>
2023-08-08 11:20:40 +0300

    test: add volto-listing-block to transformIgnorePattern - refs #254313

--- jest-addon.config.js
+++ jest-addon.config.js
@@ -20,6 +20,7 @@ module.exports = {
     'load-volto-addons':
       '<rootDir>/node_modules/@plone/volto/jest-addons-loader.js',
   },
+  transformIgnorePatterns: ['node_modules/(?!@eeacms)/volto-listing-block'],
   transform: {
     '^.+\\.js(x)?$': 'babel-jest',
     '^.+\\.(png)$': 'jest-file',
