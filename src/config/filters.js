4a1095bedc3970ee14728ee1e90b30223e50913e
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2023-08-18 11:29:33 +0300

    added new permanent filter, so we can exclude some documents from global search

--- src/config/filters.js
+++ src/config/filters.js
@@ -1,30 +1,34 @@
 import { getTodayWithTime } from '../utils';
+function constantScore() {
+  return {
+    constant_score: {
+      filter: {
+        bool: {
+          should: [
+            {
+              bool: {
+                must_not: {
+                  exists: {
+                    field: 'issued',
+                  },
+                },
+              },
+            },
+            {
+              range: { 'issued.date': { lte: getTodayWithTime() } },
+            },
+          ],
+        },
+      },
+    },
+  };
+}
+constantScore.id = 'constantScore';
 
 export default {
   // filter values that are always added to the ES requests
   permanentFilters: [
     { term: { hasWorkflowState: 'published' } },
-    () => ({
-      constant_score: {
-        filter: {
-          bool: {
-            should: [
-              {
-                bool: {
-                  must_not: {
-                    exists: {
-                      field: 'issued',
-                    },
-                  },
-                },
-              },
-              {
-                range: { 'issued.date': { lte: getTodayWithTime() } },
-              },
-            ],
-          },
-        },
-      },
-    }),
+    constantScore,
   ],
 };
--- src/config/global-search-config.js
+++ src/config/global-search-config.js
@@ -9,4 +9,20 @@ globalSearchConfig.facets = globalSearchConfig.facets.filter(
   (facet) => facet['field'] !== 'subject.keyword',
 );
 
+// customize permanent filters
+const index = globalSearchConfig.permanentFilters.findIndex(
+  (f) => f.id === 'constantScore',
+);
+const baseConstantScore = globalSearchConfig.permanentFilters[index];
+
+globalSearchConfig.permanentFilters[index] = () => {
+  const base = baseConstantScore();
+  base.constant_score.filter.bool.must_not = {
+    exists: {
+      field: 'exclude_from_globalsearch',
+    },
+  };
+  return base;
+};
+
 export default globalSearchConfig;
