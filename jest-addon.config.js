de21970e154e0936fe5331302c7fc21c0a87faa8
ana-oprea <anaop3212@gmail.com>
2024-04-19 13:18:14 +0300

    test: modify @eeacms/search alias path

--- jest-addon.config.js
+++ jest-addon.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: __dirname + '/.env' })
+require('dotenv').config({ path: __dirname + '/.env' });
 
 module.exports = {
   testMatch: ['**/src/addons/**/?(*.)+(spec|test).[jt]s?(x)'],
@@ -14,8 +14,9 @@ module.exports = {
     '@package/(.*)$': '<rootDir>/node_modules/@plone/volto/src/$1',
     '@root/(.*)$': '<rootDir>/node_modules/@plone/volto/src/$1',
     '@plone/volto-quanta/(.*)$': '<rootDir>/src/addons/volto-quanta/src/$1',
-    '@eeacms/search/(.*)$': '<rootDir>/src/addons/volto-searchlib/searchlib/$1',
-    '@eeacms/search': '<rootDir>/src/addons/volto-searchlib/searchlib',
+    '@eeacms/search/(.*)$':
+      '<rootDir>/node_modules/@eeacms/volto-searchlib/src/$1',
+    '@eeacms/search': '<rootDir>/node_modules/@eeacms/volto-searchlib/src',
     '@eeacms/(.*?)/(.*)$': '<rootDir>/node_modules/@eeacms/$1/src/$2',
     '@plone/volto-slate$':
       '<rootDir>/node_modules/@plone/volto/packages/volto-slate/src',
@@ -48,4 +49,4 @@ module.exports = {
       '<rootDir>/node_modules/@eeacms/volto-globalsearch/jest.setup.js',
     ],
   }),
-}
+};
