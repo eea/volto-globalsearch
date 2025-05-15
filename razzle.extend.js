e50f9eb600a482ce7847f8847366277ce7c775e4
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-05-15 09:15:41 +0300

    Remove tweak for pngs

--- razzle.extend.js
+++ razzle.extend.js
@@ -1,30 +1,30 @@
-const makeLoaderFinder = require('razzle-dev-utils/makeLoaderFinder');
-const fileLoaderFinder = makeLoaderFinder('file-loader');
+// const makeLoaderFinder = require('razzle-dev-utils/makeLoaderFinder');
+// const fileLoaderFinder = makeLoaderFinder('file-loader');
 
 const plugins = (defaultPlugins) => {
   return defaultPlugins;
 };
 
 const modify = (config, { target, dev }, webpack) => {
-  const fileLoader = config.module.rules.find(fileLoaderFinder);
-
-  fileLoader.exclude = [
-    /\.(config|variables|overrides)$/,
-    /icons\/.*\.svg$/,
-    /\.html$/,
-    /\.(js|jsx|mjs)$/,
-    /\.(ts|tsx)$/,
-    /\.(vue)$/,
-    /\.(less)$/,
-    /\.(re)$/,
-    /\.(s?css|sass)$/,
-    /\.json$/,
-    /\.bmp$/,
-    /\.gif$/,
-    /\.jpe?g$/,
-    // .png files we don't want them in the bundle
-    /static\/\.png$/,
-  ];
+  // const fileLoader = config.module.rules.find(fileLoaderFinder);
+  //
+  // fileLoader.exclude = [
+  //   /\.(config|variables|overrides)$/,
+  //   /icons\/.*\.svg$/,
+  //   /\.html$/,
+  //   /\.(js|jsx|mjs)$/,
+  //   /\.(ts|tsx)$/,
+  //   /\.(vue)$/,
+  //   /\.(less)$/,
+  //   /\.(re)$/,
+  //   /\.(s?css|sass)$/,
+  //   /\.json$/,
+  //   /\.bmp$/,
+  //   /\.gif$/,
+  //   /\.jpe?g$/,
+  //   // .png files we don't want them in the bundle
+  //   /static\/\.png$/,
+  // ];
 
   return config;
 };
